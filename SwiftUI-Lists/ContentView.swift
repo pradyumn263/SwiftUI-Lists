c67bc2c0e8ddb72ea2e67a0e94fd00c04b728de8
Pradyumn Shukla <ps811876@student.nitw.ac.in>
2021-01-12 22:05:31 +0530

    Initial Commit

---
+++ SwiftUI-Lists/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Lists
+//
+//  Created by Pradyumn Shukla on 12/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Lists/SwiftUI_ListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ListsApp.swift
+//  SwiftUI-Lists
+//
+//  Created by Pradyumn Shukla on 12/01/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
